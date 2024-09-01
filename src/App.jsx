ab279517a699680de7b090c80a2a269ed4715217
gastonruiz <gastonruizojeda@gmail.com>
2024-09-01 17:19:23 -0300

    se creo la landing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HomeSection from './components/HomeSection'
+import Nosotras from './components/Nosotras'
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <HomeSection />
+    <Nosotras />
+    </>
+  
+  )
+}
+
+export default App
---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+const HomeSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center">
+        TipoArche recursos y herramientas tecnicas 
+        <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'>
+      {' '} para artistas
+        </span>
+      </h1>
+      <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+        Desarrolla habilidades profesionales con los cursos que ofrecemos en TipoArche,
+        lleva tu tecnica artistica a otro nivel, construye una mirada critica sobre tu 
+        propio trbajo artistico
+      </p>
+      <div className='flex mt-10 justify/center'>
+      <video 
+      autoPlay 
+      loop
+      muted 
+      className='rounded-lg w-1/2 border border-orange-700
+      shadow-orange-400 mx-2 my-4'>
+        <source src={video1} type='video/mp4'/>
+        Your browser not support the video tag.
+      </video>
+      <video 
+      autoPlay 
+      loop
+      muted 
+      className='rounded-lg w-1/2 border border-orange-700
+      shadow-orange-400 mx-2 my-4'>
+        <source src={video2} type='video/mp4'/>
+        Your browser not support the video tag.
+      </video>
+      </div>
+    </div>
+  )
+}
+
+export default HomeSection
