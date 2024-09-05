7aef50d5ac7146f95b7b950d151123d8c3bb8dc9
gastonruiz <gastonruizojeda@gmail.com>
2024-09-05 14:55:05 -0300

    se agregaron las fotos de usuarios de perfil

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,40 +16,40 @@ export const navItems = [
 
 export const testimonials = [
   {
-    user: "John Doe",
-    company: "Stellar Solutions",
+    user: "Juan",
+    company: "Excelente material",
     image: user1,
-    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+    text: "Encontre mucha informacion util a la hora de mejorar mis habilidades como dibujante.",
   },
   {
-    user: "Jane Smith",
-    company: "Blue Horizon Technologies",
+    user: "Ana",
+    company: "Nuevos recursos",
     image: user2,
-    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+    text: "Esperaba mejorar mis tecnicas al dibujar y gane mucho mas que solo eso",
   },
   {
-    user: "David Johnson",
-    company: "Quantum Innovations",
+    user: "David",
+    company: "Innovacion en aprendizaje",
     image: user3,
-    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+    text: "Trabajando en conjunto con las profes , mejore considerablemente mis productos finales de ilustracion, lo que aumento mis ventas.",
   },
   {
-    user: "Ronee Brown",
-    company: "Fusion Dynamics",
+    user: "Rosa",
+    company: "Feedback dinamico",
     image: user4,
-    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+    text: "Obtuve respuestas rapidas y satisfactorias para mejorar de manera paulatina y consisa, entendiendo mi propio progreso",
   },
   {
-    user: "Michael Wilson",
-    company: "Visionary Creations",
+    user: "Connie",
+    company: "Creatividad sin limites",
     image: user5,
-    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+    text: "Cambiando la mentalidad de obtener un resultado especifico, aprendi a ser mas abierta a las posibilidades que me brinda la obra en si misma.",
   },
   {
-    user: "Emily Davis",
-    company: "Synergy Systems",
+    user: "Emilia",
+    company: "Autoexploracion del arte",
     image: user6,
-    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+    text: "Liuego de una serie de preguntas orientadas a mi trabajo, pude obtener una definicion del misma, lo que me ayudo a direccionarlo hacia donde yo realmente queria.",
   },
 ];
 
@@ -92,82 +92,4 @@ export const features = [
   },
 ];
 
-export const checklistItems = [
-  {
-    title: "Code merge made easy",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
-  },
-  {
-    title: "Review code without worry",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
-  },
-  {
-    title: "AI Assistance to reduce time",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
-  },
-  {
-    title: "Share work in minutes",
-    description:
-      "Track the performance of your VR apps and gain insights into user behavior.",
-  },
-];
 
-export const pricingOptions = [
-  {
-    title: "Free",
-    price: "$0",
-    features: [
-      "Private board sharing",
-      "5 Gb Storage",
-      "Web Analytics",
-      "Private Mode",
-    ],
-  },
-  {
-    title: "Pro",
-    price: "$10",
-    features: [
-      "Private board sharing",
-      "10 Gb Storage",
-      "Web Analytics (Advance)",
-      "Private Mode",
-    ],
-  },
-  {
-    title: "Enterprise",
-    price: "$200",
-    features: [
-      "Private board sharing",
-      "Unlimited Storage",
-      "High Performance Network",
-      "Private Mode",
-    ],
-  },
-];
-
-export const resourcesLinks = [
-  { href: "#", text: "Getting Started" },
-  { href: "#", text: "Documentation" },
-  { href: "#", text: "Tutorials" },
-  { href: "#", text: "API Reference" },
-  { href: "#", text: "Community Forums" },
-];
-
-export const platformLinks = [
-  { href: "#", text: "Features" },
-  { href: "#", text: "Supported Devices" },
-  { href: "#", text: "System Requirements" },
-  { href: "#", text: "Downloads" },
-  { href: "#", text: "Release Notes" },
-];
-
-export const communityLinks = [
-  { href: "#", text: "Events" },
-  { href: "#", text: "Meetups" },
-  { href: "#", text: "Conferences" },
-  { href: "#", text: "Hackathons" },
-  { href: "#", text: "Jobs" },
-];
